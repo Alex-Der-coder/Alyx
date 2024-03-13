47f05c4f3384278255b61a5dd1a4fa10886e7c1c
Alexandre De Roeck <Mr.Alexandre.deroeck@gmail.comm>
2024-03-13 11:49:16 +0100

    add type for rootlayoutprops for themeprovider

--- pages/layout.tsx
+++ pages/layout.tsx
@@ -1,6 +1,8 @@
 import { ThemeProvider } from "../@/components/ui/theme-provider"
 
-export default function RootLayout({ children } : RootLayoutProps) {
+type RootLayoutProps =  any
+
+export default function RootLayout({ children } : RootLayoutProps ) {
   return (
 
         <ThemeProvider
