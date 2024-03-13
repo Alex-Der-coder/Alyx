1c2b1091131b7db274a51970c53b03f2469aed21
Alexandre De Roeck <Mr.Alexandre.deroeck@gmail.comm>
2024-03-13 11:54:32 +0100

    add default to export for navigation and accordion component

--- pages/components/NavigationMenuDemo.tsx
+++ pages/components/NavigationMenuDemo.tsx
@@ -53,7 +53,7 @@ const components: { title: string; href: string; description: string }[] = [
   },
 ]
 
-export function NavigationMenuDemo() {
+export default function NavigationMenuDemo() {
   return (
     <NavigationMenu>
       <NavigationMenuList>
