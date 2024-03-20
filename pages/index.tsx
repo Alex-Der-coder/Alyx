de42ddc075228cd783939238111ee034fa35555c
Alexandre De Roeck <Mr.Alexandre.deroeck@gmail.comm>
2024-03-20 22:46:38 +0100

    fix index Title for responsive at 320 to 640 px

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,8 +18,8 @@ export default function Home() {
   return (
     <>
     <NavigationMenu />
-      <section className="space-y-6 pb-8 pt-6 md:pb-12 md:pt-10 lg:py-32">
-        <div className="container flex max-w-[64rem] flex-col items-center gap-4 text-center">
+      <section className="space-y-6 pb-8 pt-6 md:pb-12 md:pt-10 lg:py-32  ">
+        <div className="container flex max-w-[64rem] flex-col items-center gap-4 text-center ">
           <Link
             href="https://twitter.com/shadcn"
             className="rounded-2xl bg-muted px-4 py-1.5 text-sm font-medium"
@@ -27,7 +27,7 @@ export default function Home() {
           >
             Follow along on Twitter
           </Link>
-          <h1 className="font-heading text-3xl sm:text-5xl md:text-6xl lg:text-7xl">
+          <h1 className="font-heading text-3xl sm:text-5xl md:text-6xl lg:text-7xl max-[645px]:mt-[40%] " >
             An example app built using Next.js 13 server components.
           </h1>
           <p className="max-w-[42rem] leading-normal text-muted-foreground sm:text-xl sm:leading-8">
