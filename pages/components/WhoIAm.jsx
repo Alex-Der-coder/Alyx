8c05cb0fd98f6054c0c4b24e5f28f7e0df02080f
Alexandre De Roeck <Mr.Alexandre.deroeck@gmail.comm>
2024-04-09 18:00:17 +0200

    hotfix one : section size at mobile 320px

--- pages/components/WhoIAm.jsx
+++ pages/components/WhoIAm.jsx
@@ -9,7 +9,7 @@ import Calendar from "../components/CalendarDemo"
 const WhoIAm = () => {
 
   return (
-    <section className="h-[28rem] pt-[7rem] max-[640px]:grid   max-[640px]:justify-center  max-[640px]:h-auto " id="whoIAmSection">
+    <section className="h-[28rem] pt-[7rem] max-[640px]:grid   max-[640px]:justify-center  max-[640px]:h-auto  max-[640px]:w-[23%]" id="whoIAmSection">
       <div className="flex flex-row justify-around max-[640px]:flex-col   max-[640px]:h-[150%]  max-[640px]:items-center ">
       <SpeedInsights/>
       <CardAboutMee />
