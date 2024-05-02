02c179fa865c04caf95bd0006c9fa79c51c22fcc
Alexandre De Roeck <Mr.Alexandre.deroeck@gmail.comm>
2024-05-02 09:49:35 +0200

    del inused code

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,17 +11,6 @@ class MyDocument extends Document {
             content="Explore my portfolio showcasing innovative JavaScript/React applications, crafted to deliver seamless user experiences and cutting-edge functionalities."
           />
           <meta property="og:site_name" content="nextjsconf-pics.vercel.app" />
-          <meta
-            property="og:description"
-            content="See pictures from Next.js Conf and the After Party."
-          />
-          <meta property="og:title" content="Next.js Conf 2022 Pictures" />
-          <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:title" content="Next.js Conf 2022 Pictures" />
-          <meta
-            name="twitter:description"
-            content="See pictures from Next.js Conf and the After Party."
-          />
            <meta name="google-site-verification" content="CRtevFl6lPGgVDHwPqg65cz5nBCSRlPtB9eSuArYMT8" />
         </Head>
         <body>
