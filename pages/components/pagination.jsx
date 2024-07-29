168d99227054ade334c4e82507e4c9f370c85b2f
Alexandre De Roeck <Mr.Alexandre.deroeck@gmail.comm>
2024-07-29 23:18:44 +0200

    fixes issue #3

--- pages/components/pagination.jsx
+++ pages/components/pagination.jsx
@@ -8,7 +8,7 @@ import {
     PaginationPrevious,
   } from "../../@/components/ui/pagination"
   
-  export function PaginationDemo() {
+  export default function PaginationDemo() {
     return (
       <Pagination>
         <PaginationContent>
